ed4d2910f42caadb88b374323f810e34d54f92f4
michal <michalwagh@gmail.com>
2022-10-26 18:07:15 +0100

    carousel seeting addjusteed

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "./Carousel.scss"
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import { Link } from 'react-router-dom'
 
 import React from 'react'
+
 
 const Carousel = ({ cars }) => {
 
@@ -24,32 +25,53 @@ const Carousel = ({ cars }) => {
         )
     }
 
+
+
     const [imageIndex, setImageIndex] = useState(0)
 
+
+
     const setting = {
-        infinite: true,
+
         lazyload: true,
-        speed: 300,
+
         slidesToShow: 3,
+
         centerMode: true,
         centerPadding: 0,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        beforeChange: (current, next) => setImageIndex(next)
+
+        beforeChange: (current, next) => setImageIndex(next),
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        // cssEase: "linear"
     }
 
+    // if (!imageIndex) {
+    //     imageIndex = 0
+    // }
 
-
+    console.log(imageIndex);
     return (
         <div className="carousel">
             <Slider {...setting} className="carousel__slider" >
-                {cars.map((car, index) => (
+                {
 
-                    <Link to={`/car/${car.id}`} key={car.id} className={index == imageIndex ? "slide activeSlide" : "slide"}>
-                        <img className="carousel__img" src={car.img_url} alt={car.img_url} />
-                    </Link>
+                    cars.map((car, index) => (
 
-                ))}
+
+                        <Link to={`/car/${car.id}`} key={car.id}
+                            className={
+                                index == imageIndex ? "slide activeSlide" : "slide"}
+                        >
+                            <img className="carousel__img" src={car.img_url} alt={car.img_url} ></img>
+                        </Link>
+
+                    ))}
             </Slider>
         </div>
     )

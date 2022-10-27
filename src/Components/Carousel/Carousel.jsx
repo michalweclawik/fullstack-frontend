b477b38303447a50e77c29dc3a03e0c782adf245
michal <michalwagh@gmail.com>
2022-10-27 11:54:23 +0100

    descrption on img added

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Slider from "react-slick";
 import "./Carousel.scss"
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
 import { Link } from 'react-router-dom'
 
 import React from 'react'
@@ -10,20 +10,6 @@ import React from 'react'
 const Carousel = ({ cars }) => {
 
 
-    const NextArrow = ({ onClick }) => {
-        return (
-            <div className="arrow next" onClick={onClick}>
-                <FaArrowRight />
-            </div>
-        )
-    }
-    const PrevArrow = ({ onClick }) => {
-        return (
-            <div className="arrow prev" onClick={onClick}>
-                <FaArrowLeft />
-            </div>
-        )
-    }
 
 
 
@@ -32,44 +18,30 @@ const Carousel = ({ cars }) => {
 
 
     const setting = {
-
         lazyload: true,
-
         slidesToShow: 3,
-
         centerMode: true,
         centerPadding: 0,
-
         beforeChange: (current, next) => setImageIndex(next),
         dots: true,
         infinite: true,
-        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         speed: 2000,
         autoplaySpeed: 2000,
-        // cssEase: "linear"
     }
 
-    // if (!imageIndex) {
-    //     imageIndex = 0
-    // }
+
 
     console.log(imageIndex);
     return (
         <div className="carousel">
             <Slider {...setting} className="carousel__slider" >
                 {
-
-                    cars.map((car, index) => (
-
-
-                        <Link to={`/car/${car.id}`} key={car.id}
-                            className={
-                                index == imageIndex ? "slide activeSlide" : "slide"}
-                        >
-                            <img className="carousel__img" src={car.img_url} alt={car.img_url} ></img>
-                        </Link>
+                    cars.map((car, index) => (<Link to={`/car/${car.id}`} key={car.id} className={index === imageIndex ? "slide activeSlide" : "slide"}>
+                        <img className="carousel__img" src={car.img_url} alt={car.img_url} ></img>
+                        {index === imageIndex && <h6 >{car.model}</h6>}
+                    </Link>
 
                     ))}
             </Slider>
